7e14a80c6e84d52f62fece59aff4b279adff97d2
venom <abdo30201080101694@gmail.com>
2024-03-28 11:48:59 +0200

    handle the animation of the bars

--- src/components/SkillItem.js
+++ src/components/SkillItem.js
@@ -2,20 +2,24 @@ import React from "react";
 import "../assets/style/skill-item.css";
 
 export default function SkillItem({ skillName, skillLevel }) {
+  // Parse the skillLevel string to a percentage value
+  const percentage = parseFloat(skillLevel);
+
   return (
     <div className="skill-item">
       <div className="skill-name-container">
         <div className="skill-name">{skillName}</div>
         <div className="level">{skillLevel}</div>
       </div>
-
       <div className="skill-level-container">
         <div
           className="skill-level"
           style={{
-            width: `${skillLevel}`,
-            height: "100%",
+            "--skill-width": percentage,
           }}
+          role="progressbar"
+          data-aos="zoom-in-right"
+          data-aos-delay="100"
         ></div>
       </div>
     </div>
