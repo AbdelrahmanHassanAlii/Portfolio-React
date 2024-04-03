7ad1bc730c0776b9e92a2c6c9888b692b3bbd68a
venom <abdo30201080101694@gmail.com>
2024-04-03 19:36:19 +0200

    handle another css for contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../assets/style/contact.css";
+import { FaTelegramPlane } from "react-icons/fa";
 
 export default function Contact() {
   return (
@@ -8,34 +9,31 @@ export default function Contact() {
         <h1>Get in touch</h1>
         <span>Do you have a project in your mind, contact me here</span>
       </div>
-      <div class="row">
-        <div class="col">
-          <div class="contact-info">
-            <h2>
-              Find Me <i class="uil uil-corner-right-down"></i>
-            </h2>
-            <p>
-              <i class="uil uil-envelope"></i> Email: john@doe.com
-            </p>
-            <p>
-              <i class="uil uil-phone"></i> +91 70822 00001
-            </p>
-          </div>
+      <div class="contact-container">
+        <div class="contact-info">
+          <h2>
+            Find Me <i class="uil uil-corner-right-down"></i>
+          </h2>
+          <p>
+            <i class="uil uil-envelope"></i> Email: john@doe.com
+          </p>
+          <p>
+            <i class="uil uil-phone"></i> +91 70822 00001
+          </p>
         </div>
-        <div class="col">
-          <div class="form-control">
-            <div class="form-inputs">
-              <input type="text" class="input-field" placeholder="Name" />
-              <input type="text" class="input-field" placeholder="Email" />
-            </div>
-            <div class="text-area">
-              <textarea placeholder="Message"></textarea>
-            </div>
-            <div class="form-button">
-              <button class="btn">
-                Send <i class="uil uil-message"></i>
-              </button>
-            </div>
+        <div class="form-control">
+          <div class="form-inputs">
+            <input type="text" class="input-field" placeholder="Name" />
+            <input type="text" class="input-field" placeholder="Email" />
+          </div>
+          <div class="text-area">
+            <textarea placeholder="Message"></textarea>
+          </div>
+          <div class="form-button">
+            <button class="btn">
+              <span class="btn-text">Send</span>
+              <FaTelegramPlane className="btn-icon" fontSize={"18px"} />
+            </button>
           </div>
         </div>
       </div>
