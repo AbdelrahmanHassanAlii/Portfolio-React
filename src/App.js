2c6e88cba45f516261707dbc7b92a5b5f84a3211
venom <abdo30201080101694@gmail.com>
2024-04-03 19:24:00 +0200

    add contact section and it,s main css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Contact from "./components/Contact";
 import Header from "./components/Header";
 import Landing from "./components/Landing";
 import Projects from "./components/Projects";
@@ -11,6 +12,7 @@ function App() {
       <Landing />
       <AboutMe />
       <Projects />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/Contact.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "../assets/style/contact.css";
+
+export default function Contact() {
+  return (
+    <div className="contact-section narrow-container" id="contact">
+      <div class="top-header">
+        <h1>Get in touch</h1>
+        <span>Do you have a project in your mind, contact me here</span>
+      </div>
+      <div class="row">
+        <div class="col">
+          <div class="contact-info">
+            <h2>
+              Find Me <i class="uil uil-corner-right-down"></i>
+            </h2>
+            <p>
+              <i class="uil uil-envelope"></i> Email: john@doe.com
+            </p>
+            <p>
+              <i class="uil uil-phone"></i> +91 70822 00001
+            </p>
+          </div>
+        </div>
+        <div class="col">
+          <div class="form-control">
+            <div class="form-inputs">
+              <input type="text" class="input-field" placeholder="Name" />
+              <input type="text" class="input-field" placeholder="Email" />
+            </div>
+            <div class="text-area">
+              <textarea placeholder="Message"></textarea>
+            </div>
+            <div class="form-button">
+              <button class="btn">
+                Send <i class="uil uil-message"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
