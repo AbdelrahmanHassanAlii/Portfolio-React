2e79d2143bae24899b8adeecff124364305f53d2
venom <abdo30201080101694@gmail.com>
2024-04-05 14:41:06 +0200

    refactor the social icon componet and let get the link to go to

--- src/components/Social.js
+++ src/components/Social.js
@@ -8,9 +8,20 @@ import SocialIcon from "./SocialIcon";
 export default function Social() {
   return (
     <div className="social-icons">
-      <SocialIcon icon={<FaFacebookF color="#1877F2" />} />
-      <SocialIcon icon={<FaLinkedinIn color="#0077B5" />} />
-      <SocialIcon icon={<FaGithub color="#fff" />} />
+      <SocialIcon
+        icon={<FaFacebookF color="#1877F2" />}
+        link={"https://www.facebook.com/profile.php?id=100015463386226"}
+      />
+      <SocialIcon
+        icon={<FaLinkedinIn color="#0077B5" />}
+        link={
+          "https://www.linkedin.com/in/abdelrahman-hassan-b09b80250?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Bf4hU2ZwhSB6eg8sfKk0Olg%3D%3D"
+        }
+      />
+      <SocialIcon
+        icon={<FaGithub color="#fff" />}
+        link={"https://github.com/AbdelrahmanHassanAlii"}
+      />
     </div>
   );
 }
--- src/components/SocialIcon.js
+++ src/components/SocialIcon.js
@@ -1,12 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export default function SocialIcon({ icon }) {
+export default function SocialIcon({ icon, link }) {
   return (
-    <button className="iconButton">
-      <span className="iconContainer">
-        {icon}
-      </span>
-      <span className="back-ground"></span>
-    </button>
+    <Link to={link} target="_blank" rel="noopener noreferrer">
+      <button className="iconButton">
+        <span className="iconContainer">{icon}</span>
+        <span className="back-ground"></span>
+      </button>
+    </Link>
   );
 }
