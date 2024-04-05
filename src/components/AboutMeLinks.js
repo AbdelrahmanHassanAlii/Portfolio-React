c2d849e8594e2cc563039101f8efedfd9f7a8662
venom <abdo30201080101694@gmail.com>
2024-04-06 01:26:57 +0200

    refactor the set active link on about me section

--- src/components/AboutMeLinks.js
+++ src/components/AboutMeLinks.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../assets/style/about-me-links.css";
 
 export default function AboutMeLinks() {
+  const [activeLink, setActiveLink] = useState("/aboutme/about");
+
+  const handleClick = (to) => {
+    setActiveLink(to);
+  };
+
   return (
     <div className="about-me-links">
-      <Link to="/aboutme/about">About</Link>
-      <Link to="/aboutme/skills">Skills</Link>
-      <Link to="/aboutme/services">Services</Link>
-      {/* <Link to="/aboutme/education">Education</Link>
-      <Link to="/aboutme/award">Award</Link> */}
+      <Link
+        to="/aboutme/about"
+        className={activeLink === "/aboutme/about" ? "active" : ""}
+        onClick={() => handleClick("/aboutme/about")}
+      >
+        About
+      </Link>
+      <Link
+        to="/aboutme/skills"
+        className={activeLink === "/aboutme/skills" ? "active" : ""}
+        onClick={() => handleClick("/aboutme/skills")}
+      >
+        Skills
+      </Link>
+      <Link
+        to="/aboutme/services"
+        className={activeLink === "/aboutme/services" ? "active" : ""}
+        onClick={() => handleClick("/aboutme/services")}
+      >
+        Services
+      </Link>
     </div>
   );
 }
