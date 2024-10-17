4b4b6df084ac7f269c386a56fce29d535a4f2d35
unknown <abdo30201080101694@gmail.com>
2024-10-17 17:44:23 +0300

    handle the basic structure of product details

--- src/pages/ProjectDetail.jsx
+++ src/pages/ProjectDetail.jsx
@@ -4,6 +4,7 @@ import projectsData from "../data/Json Folder/projects.json";
 import "../assets/style/project-details.css";
 export default function ProjectDetail() {
   const [projectDetails, setProjectDetails] = useState({});
+  const [baseimageLink, setBaseImageLink] = useState("");
   const { id } = useParams();
 
   useEffect(() => {
@@ -11,23 +12,48 @@ export default function ProjectDetail() {
       let projects = projectsData;
       let project = projects.find((project) => project.id === id);
       setProjectDetails(project);
-      console.log(project);
+      setBaseImageLink(project.image_1);
     }
   }, [id]);
+
+  const handleImageChange = (image) => {
+    setBaseImageLink(image);
+  };
+
   return (
     <div className="project-detail-page">
-      <div className="project-detail-container"></div>
-      <div className="project-detail-img">
-        <img
-          src={projectDetails.image_1}
-          alt={projectDetails.title}
-        />
-      </div>
-      <div className="project-detail-info">
-        <h1 className="project-detail-title">{projectDetails.title}</h1>
-        <p className="project-detail-description">
-          {projectDetails.description}
-        </p>
+      <div className="project-detail-content">
+        <div className="project-detail-images">
+          <div className="main-image">
+            <img
+            src={baseimageLink}
+            alt={projectDetails.title}
+          />
+          </div>
+          <div className="images">
+            <img
+              src={projectDetails.image_1}
+              alt={projectDetails.title}
+              onClick={() => handleImageChange(projectDetails.image_1)}
+            />
+            <img
+              src={projectDetails.image_2}
+              alt={projectDetails.title}
+              onClick={() => handleImageChange(projectDetails.image_2)}
+            />
+            <img
+              src={projectDetails.image_3}
+              alt={projectDetails.title}
+              onClick={() => handleImageChange(projectDetails.image_3)}
+            />
+          </div>
+        </div>
+        <div className="project-detail-info">
+          <h1 className="project-detail-title">{projectDetails.title}</h1>
+          <p className="project-detail-description">
+            {projectDetails.description}
+          </p>
+        </div>
       </div>
     </div>
   );
